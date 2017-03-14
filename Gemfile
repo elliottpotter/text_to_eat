1faf93f296b1fe0b7999e75018493678b5f0d8ed
Elliott Potter <elliott@shipt.com>
2017-03-14 22:23:57 +0100

    Version one

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'pry-byebug'
+gem 'mechanize'
+gem 'nokogiri'
 gem 'figaro'
 gem 'twilio-ruby', '~> 4.11.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,19 +1,41 @@
+require 'mechanize'
+
 class QueriesController < ApplicationController
+  
 
   def create
-    if User.where(phone_number: params[:from_number])
+    if User.where(phone_number: params[:from])
       @user = User.where(phone_number: params[:from_number])
     else
       @user = User.create(phone_number: params[:phone_number])
     end
+    @query = Query.create(user: @user, query: params[:body].strip.lstrip)
+    find_restaurants(@query)
+  end
 
-    @query = Query.create(user: @user, query: params[:body])
-    code               = @driver.verification_code
+
+  def find_restaurants(query)
+    query = (query.split.each {|w| w.capitalize!}).join(' ')
+    agent = Mechanize.new
+    first_page = agent.get("https://www.tripadvisor.com/Restaurants.com")
+    first_page.forms[0]["q"] = query
+    second_page = first_page.forms[0].submit
+    restaurants = []
+    city = query.split.first.lower.capitalize
+    restaurant_page = second_page.link_with(href: /(Restaurants).*(city)/).click
+    restaurant_page.search(".property_title").first(5).each { |res| restaurants << res.text.gsub(/\n/, '') }
+    send_results(restaurants)
+  end
+
+
+  def send_results(restaurants)
     @client = Twilio::REST::Client.new ENV["twilio_account_sid"], ENV["twilio_auth_token"]
     @client.messages.create(
       from: '+12052895050',
-      to:   '+12178163505',
-      body: "This is elliott from a test app, let me know if you get dits!"
+      to:   @user.phone_number,
+      body: restaurants.each_with_index { |restaurant, i| puts "#{i + 1}. #{restaurant}" }
     )
   end
+
+
 end
