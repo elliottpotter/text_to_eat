8dec629c0d5507f3e48a92dd527972ff127024f4
Elliott Potter <elliott@shipt.com>
2017-07-30 17:25:15 -0500

    Cookie controller fixed

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -6,12 +6,16 @@ class CookiesController < ApplicationController
     AllflicksCookie.create(
       phpsessid:  cookie_params[:phpsessid],
       identifier: cookie_params[:identifier]
-      )
+    )
   end
 
   private
 
   def cookie_params
+    cookies = {}
+    params[:cookies].map { |cookie| cookies[cookie[:name]] = cookie[:value] if cookie[:name] == "PHPSESSID" || cookie[:name] == "identifier" }
+    params[:phpsessid]  = cookies["PHPSESSID"]
+    params[:identifier] = cookies["identifier"]
     params.permit(:phpsessid, :identifier)
   end
 
